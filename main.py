24042f67f7b3d66f5d241181ce090a38405cd690
MarkSmith26 <mark_smith26@my.vcccd.edu>
2023-08-14 15:30:24 -0700

    Create main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-print ("Hello World")
-print ("CS7: Pyhton Programming")
+    print ("Hello World")
+    print ("CS7:Python Programming")
 
 if __name__ == '__main__':
     main()
